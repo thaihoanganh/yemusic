28f38933cd6c81694c0b3e3ab448adea8c3b3a5e
hoanganhthai <hoanganhthai161299@gmail.com>
2023-02-23 23:36:08 +0700

    feat: update example

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -1,16 +1,58 @@
-import { Paper } from '@yemusic/components';
-import { ThemeProvider } from '@yemusic/components';
+import { Paper, StateLayer, TextHeadline, ThemeProvider, UnstyledButton, useTheme } from '@yemusic/components';
+
+function getRandomHexColor() {
+	const hexChars = '0123456789ABCDEF';
+	let hex = '#';
+	for (let i = 0; i < 6; i++) {
+		hex += hexChars[Math.floor(Math.random() * 16)];
+	}
+	return hex;
+}
 
 const index = () => {
+	const { onSetThemeFromSourceColor } = useTheme();
+
 	return (
 		<ThemeProvider>
-			<Paper
-				style={{
-					width: '100vw',
-					height: '100vh',
-				}}
-				background="background"
-			></Paper>
+			<StateLayer color="secondary-container" state={['hover', 'pressed']}>
+				{({ isHover, isPressed }) => (
+					<Paper
+						style={{
+							display: 'flex',
+							justifyContent: 'center',
+							gap: '2rem',
+							alignItems: 'center',
+							width: '100vw',
+							height: '100vh',
+						}}
+						background="secondary-container"
+					>
+						<UnstyledButton
+							onClick={() => {
+								onSetThemeFromSourceColor({
+									dynamicColor: getRandomHexColor(),
+									primaryColor: getRandomHexColor(),
+								});
+							}}
+						>
+							<TextHeadline color="on-secondary-container">Dynamic Theme, Click !!!</TextHeadline>
+						</UnstyledButton>
+
+						<Paper
+							style={{
+								display: 'flex',
+								justifyContent: 'center',
+								alignItems: 'center',
+								width: '15rem',
+								height: '15rem',
+							}}
+							background="tertiary-container"
+						>
+							<TextHeadline color="on-tertiary-container">Primary Container</TextHeadline>
+						</Paper>
+					</Paper>
+				)}
+			</StateLayer>
 		</ThemeProvider>
 	);
 };
