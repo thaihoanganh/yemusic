6c7b45e44f00bd15f88f097c6e80615066502230
hoanganhthai <hoanganhthai161299@gmail.com>
2023-03-12 03:24:53 +0700

    refactor(desktop-sidebar): change menu link

--- common/components/src/organisms/DesktopSidebar/DesktopSidebar.tsx
+++ common/components/src/organisms/DesktopSidebar/DesktopSidebar.tsx
@@ -1,26 +1,13 @@
 import Link from 'next/link';
 
-import { UnstyledButton } from '../../atoms/Button';
-import Frame, { Group, Stack } from '../../atoms/Frame';
-import {
-	FavoriteFillIcon,
-	FavoriteIcon,
-	HomeFillIcon,
-	HomeIcon,
-	InstallDesktopIcon,
-	SearchIcon,
-} from '../../atoms/Icons';
-import { Paper } from '../../atoms/Paper';
+import { Group, Stack } from '../../atoms/Frame';
+import { FavoriteFillIcon, FavoriteIcon, HomeFillIcon, HomeIcon, SearchIcon } from '../../atoms/Icons';
 import Typography from '../../atoms/Typography/Typography';
 import { NavigationDrawer, NavigationDrawerItem } from '../../molecules/NavigationDrawer';
 
 export const DesktopSidebar = () => {
 	return (
-		<Stack
-			style={{
-				height: '100vh',
-			}}
-		>
+		<Stack>
 			<Group
 				style={{
 					height: 72,
@@ -53,38 +40,13 @@ export const DesktopSidebar = () => {
 					/>
 					<NavigationDrawerItem to="/search" label="Tìm kiếm" icon={<SearchIcon />} iconActive={<SearchIcon />} />
 					<NavigationDrawerItem
-						to="/liked-songs"
+						to="/liked-tracks"
 						label="Bài hát đã thích"
 						icon={<FavoriteIcon />}
 						iconActive={<FavoriteFillIcon />}
 					/>
 				</NavigationDrawer>
 			</Stack>
-
-			<Frame horizontalPadding="large" verticalPadding="large">
-				<Frame cornerRadius="medium">
-					<Paper color="surface">
-						<Paper color="primary" surfaceLevel={3}>
-							<Stack spacing="medium" horizontalPadding="medium" verticalPadding="medium">
-								<Typography variant="body" textAlign="center">
-									Tải xuống ứng dụng của chúng tôi ngay bây giờ để trải nghiệm tất cả các tính năng.
-								</Typography>
-
-								<UnstyledButton>
-									<Frame cornerRadius="small">
-										<Paper color="primary-container">
-											<Group spacing="small" justifyContent="center" alignItems="center" verticalPadding="small">
-												<InstallDesktopIcon />
-												<Typography variant="label">Download now</Typography>
-											</Group>
-										</Paper>
-									</Frame>
-								</UnstyledButton>
-							</Stack>
-						</Paper>
-					</Paper>
-				</Frame>
-			</Frame>
 		</Stack>
 	);
 };
