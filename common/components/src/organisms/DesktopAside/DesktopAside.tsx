6906120c79c2b66bc34618150162b93a055d3399
hoanganhthai <hoanganhthai161299@gmail.com>
2023-03-19 15:38:10 +0700

    wip: implement Queue feature

---
+++ common/components/src/organisms/DesktopAside/DesktopAside.tsx
@@ -0,0 +1,82 @@
+import React, { useContext, useMemo } from 'react';
+
+import { PlayerControlsContext, QueueContext, TracksContext } from '@yemusic/providers';
+
+import { Group, Stack } from '../../atoms/Frame';
+import Typography from '../../atoms/Typography/Typography';
+import { Track } from '../Track';
+
+export const DesktopAside = () => {
+	const tracks = useContext(TracksContext.initial);
+	const { queueTrackIds, currentTrackId } = useContext(QueueContext.initial);
+	const { isPlaying } = useContext(PlayerControlsContext.initial);
+
+	const tracksQueue = useMemo(() => {
+		return tracks
+			.filter(track => queueTrackIds.includes(track.id))
+			.sort((a, b) => {
+				const aIndex = queueTrackIds.indexOf(a.id);
+				const bIndex = queueTrackIds.indexOf(b.id);
+
+				return aIndex - bIndex;
+			});
+	}, [tracks, queueTrackIds]);
+
+	const trackCurrent = useMemo(() => {
+		return tracks.find(track => track.id === currentTrackId);
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [currentTrackId]);
+
+	return (
+		<Stack spacing="medium">
+			<Group
+				style={{
+					height: 72,
+				}}
+				alignItems="center"
+				horizontalPadding="small"
+			>
+				<Typography variant="title" size="large">
+					Danh sách phát
+				</Typography>
+			</Group>
+
+			<Stack spacing="small" horizontalPadding="small">
+				<Typography variant="title">Đang phát</Typography>
+
+				<Stack>
+					{trackCurrent && (
+						<Track
+							key={trackCurrent.id}
+							title={trackCurrent.title}
+							author={trackCurrent.author}
+							thumbnail={trackCurrent.thumbnail}
+							trackId={trackCurrent.id}
+							isPlaying={isPlaying}
+							nowPlaying
+						/>
+					)}
+				</Stack>
+			</Stack>
+
+			<Stack spacing="small" horizontalPadding="small">
+				<Typography variant="title">Tiếp theo</Typography>
+
+				<Stack>
+					{tracksQueue.map(track => (
+						<Track
+							key={track.id}
+							title={track.title}
+							author={track.author}
+							thumbnail={track.thumbnail}
+							trackId={track.id}
+							isPlaying={track.isNowPlaying}
+						/>
+					))}
+				</Stack>
+			</Stack>
+		</Stack>
+	);
+};
+
+export default DesktopAside;
