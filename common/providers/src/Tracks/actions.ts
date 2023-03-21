3f70e925990bd63930a1ba79706307214b6f32c3
hoanganhthai <hoanganhthai161299@gmail.com>
2023-03-22 01:19:35 +0700

    refactor: remove track actions

--- common/providers/src/Tracks/actions.ts
+++ common/providers/src/Tracks/actions.ts
@@ -40,46 +40,6 @@ export function onToggleLikeTrack({ trackId, isLiked }: { trackId: string; isLik
 	}
 }
 
-export function onToggleNowPlayingTrack({ trackId }: { trackId: string }) {
-	const { getState, updateState } = TracksContext;
-	const tracks = getState();
-
-	const trackIndex = tracks.findIndex(track => track.id === trackId);
-
-	if (trackIndex !== -1) {
-		const track = tracks[trackIndex];
-
-		updateState(prevState => {
-			prevState[trackIndex] = {
-				...track,
-				isNowPlaying: !track.isNowPlaying,
-			};
-
-			return prevState;
-		});
-	}
-}
-
-export function toggleLoadingAudioTrack({ trackId }: { trackId: string }) {
-	const { getState, updateState } = TracksContext;
-	const tracks = getState();
-
-	const trackIndex = tracks.findIndex(track => track.id === trackId);
-
-	if (trackIndex !== -1) {
-		const track = tracks[trackIndex];
-
-		updateState(prevState => {
-			prevState[trackIndex] = {
-				...track,
-				isLoadingAudio: !track.isLoadingAudio,
-			};
-
-			return prevState;
-		});
-	}
-}
-
 export function setAudioUrlTrack({ trackId, audioUrl }: { trackId: string; audioUrl: string }) {
 	const { getState, updateState } = TracksContext;
 	const tracks = getState();
