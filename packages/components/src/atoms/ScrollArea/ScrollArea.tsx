fa7613e09df14d6bcaade265b88f2f3e1a00ff68
Hoang Anh Thai <hoanganhthai@dev.local>
2023-04-08 00:07:00 +0700

    refactor: add props & update ScrollArea component

--- packages/components/src/atoms/ScrollArea/ScrollArea.tsx
+++ packages/components/src/atoms/ScrollArea/ScrollArea.tsx
@@ -1,26 +1,46 @@
-import React, { useCallback } from 'react';
+import React, { useState } from 'react';
+
+import classNames from 'classnames';
 
 import { scrollAreaStyles } from './ScrollArea.css';
 
 export interface ScrollAreaProps extends React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement> {
 	fillContainer?: boolean;
+	visibleScrollbarOnHover?: boolean;
 }
 
-export const ScrollArea = ({ children, fillContainer, ...otherProps }: ScrollAreaProps) => {
-	const handleScrollAreaNode = useCallback(
-		(node: HTMLDivElement | null) => {
-			if (node && fillContainer) {
-				const parentHeight = node.parentElement?.clientHeight;
-				if (parentHeight) {
-					node.style.height = `${parentHeight}px`;
-				}
+export const ScrollArea = ({ children, fillContainer, visibleScrollbarOnHover, ...otherProps }: ScrollAreaProps) => {
+	const timeoutRef = React.useRef<NodeJS.Timeout | null>(null);
+	const [isHovered, setIsHovered] = useState(false);
+
+	const handleMouseEnter = () => {
+		if (visibleScrollbarOnHover) {
+			if (timeoutRef.current) {
+				clearTimeout(timeoutRef.current);
 			}
-		},
-		[fillContainer]
-	);
+			setIsHovered(true);
+		}
+	};
+
+	const handleMouseLeave = () => {
+		if (visibleScrollbarOnHover) {
+			timeoutRef.current = setTimeout(() => {
+				setIsHovered(false);
+			}, 1000);
+		}
+	};
 
 	return (
-		<div ref={handleScrollAreaNode} className={scrollAreaStyles.root} {...otherProps}>
+		<div
+			onMouseEnter={handleMouseEnter}
+			onMouseLeave={handleMouseLeave}
+			className={classNames(
+				scrollAreaStyles.root,
+				fillContainer && scrollAreaStyles.fillContainer,
+				isHovered && scrollAreaStyles.visibleScrollbar
+			)}
+			{...otherProps}
+		>
 			{children}
 		</div>
 	);
